483c1f0fcf0ccc19895897332caca6c222236bc0
Hannes Ljungberg <hannes@5monkeys.se>
2018-08-07 22:08:00 +0200

    Add brew.sh

---
+++ brew.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+# Install Bash 4.
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+brew install screen
+
+# Install other useful binaries.
+brew install ack
+brew install curl
+brew install git
+brew install imagemagick --with-webp
+brew install tree
+brew install ffmpeg --with-chromaprint --with-fdk-aac --with-libass --with-librsvg --with-libsoxr --with-libssh --with-tesseract --with-libvidstab --with-opencore-amr --with-openh264 --with-openjpeg --with-openssl --with-rtmpdump --with-rubberband --with-sdl2 --with-snappy --with-tools --with-webp --with-x265 --with-xz --with-zeromq --with-zimg
+brew install ncdu
+brew install lice
+brew install yarn
+brew install java
+brew install mtr
+brew install htop
+brew install iftop
+brew install irssi
+brew install jq
+brew install nmap
+brew install pv
+
+brew install nodejs
+brew install python
+brew install swiftlint
+brew install fastlane
+brew install carthage
+brew install cocoapods
+brew install pipenv
+
+# Remove outdated versions from the cellar.
+brew cleanup
